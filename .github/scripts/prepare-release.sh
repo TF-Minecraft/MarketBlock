5be111093beb9e3a84a11f8e3b996381b112816a
Ryan Barlow <7389646+ryanbarlow97@users.noreply.github.com>
2026-09-22 23:33:36 +0000

    fix: resolve build warnings

--- .github/scripts/prepare-release.sh
+++ .github/scripts/prepare-release.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 ref=883970ddc256f0e4c3bcaee87e6cf33171b9d4a4
 mkdir -p libs
 curl --fail --location --silent --show-error --retry 3 -H "Authorization: Bearer $GH_TOKEN" -H "Accept: application/vnd.github.raw+json" "https://api.github.com/repos/TF-Minecraft/ServerAssets/contents/jars/4241c14a7727/gson-2.10.1.jar?ref=$ref" > "libs/gson-2.10.1.jar"
-sha256sum --check .github/dependencies.sha256
+bash .github/scripts/install-local-dependencies.sh "$@"
